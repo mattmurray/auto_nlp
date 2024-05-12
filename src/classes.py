cdc653206a1fa304e3b21a0352b87cd3fb4e433b
matt <mattmurray@outlook.com>
2024-05-12 21:23:17 +0100

    Updated requirements.txt and classes.py

--- src/classes.py
+++ src/classes.py
@@ -87,7 +87,7 @@ class MultiLabelDataset(torch.utils.data.Dataset):
         self.labels = labels
 
     def __getitem__(self, idx):
-        item = {key: torch.tensor(val[idx]) for key, val in self.encodings.items()}
+        item = {key: val[idx].clone().detach() for key, val in self.encodings.items()}
         item['labels'] = torch.tensor(self.labels[idx])
         return item
 
